64ab6df10b11dc00f818323dbaa3e9852517b62f
Devilhound026 <welchj2@go.stockton.edu>
2023-09-29 11:46:51 -0400

    finish homework 2

--- HW2.py
+++ HW2.py
@@ -1,5 +1,5 @@
 # HW2.py
-# Author:
+# Author:Jonathan Welch 
 
 
 # Question 1:
@@ -8,7 +8,13 @@
 # If the age is less than 13, print "You are a child."
 # If the age is between 13 and 19, print "You are a teenager."
 # If the age is 20 or older, print "You are an adult."
-
+inputAge = int(input("Enter your age: "))
+if inputAge < 13:
+    print("You are a child.")
+elif inputAge >= 13 and inputAge <= 19:
+    print("You are a teenager.")
+else:
+    print("You are an adult.")
 
 # Question 2:
 # Write some code to display the following pattern using a for or while loop:
@@ -17,6 +23,10 @@
 # 123
 # 1234
 # 12345
+whileLoop = 1
+while whileLoop <= 5:
+    print(whileLoop)
+    whileLoop += 1
 
 # Question 3:
 # Write a Python program that prompts the user to input 10 numbers. After all the numbers are inputted, the program should display:
