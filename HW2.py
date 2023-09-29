88e3616c9c807bcd9b9fbb1b67ce0bd4475f1891
Jonathan welch <welchj2@go.stockton.edu>
2023-09-29 12:15:58 -0400

    finsih homework 2

--- HW2.py
+++ HW2.py
@@ -1,5 +1,5 @@
 # HW2.py
-# Author:
+# Author:Jonathan Welch 
 
 
 # Question 1:
@@ -8,7 +8,13 @@
 # If the age is less than 13, print "You are a child."
 # If the age is between 13 and 19, print "You are a teenager."
 # If the age is 20 or older, print "You are an adult."
-
+inputAge = int(input("Please enter your age: "))
+if inputAge < 13:
+    print("You are a child.")
+elif inputAge >= 13 and inputAge <= 19:
+    print("You are a teenager.")    
+else:    
+    print("You are an adult.")
 
 # Question 2:
 # Write some code to display the following pattern using a for or while loop:
@@ -17,14 +23,38 @@
 # 123
 # 1234
 # 12345
-
+whileLoop = 1
+while whileLoop <= 5:
+    print()
+    whileLoop += 1
+    forLoop = 1
+    for forLoop in range(1, whileLoop):
+        print(forLoop, end="")
+        breakpoint
+        
+        
+    
 # Question 3:
 # Write a Python program that prompts the user to input 10 numbers. After all the numbers are inputted, the program should display:
 
 # The highest number.
 # The lowest number.
 # The average of all the numbers.
-
+print(" ")
+inputNumber = int(input("Please enter a number: "))
+highestNumber = inputNumber
+lowestNumber = inputNumber
+totalNumber = inputNumber
+for i in range(1, 10):
+    inputNumber = int(input("Please enter a number: "))
+    totalNumber += inputNumber
+    if inputNumber > highestNumber:
+        highestNumber = inputNumber
+    if inputNumber < lowestNumber:
+        lowestNumber = inputNumber
+print("The highest number is: ", highestNumber)
+print("The lowest number is: ", lowestNumber)
+print("The average of all the numbers is: ", totalNumber/10)
 # Question 4:
 # Vowel Counter - Write some code that prompts the user to enter a string. The program should then display the number of vowels in the string. IE. If the user enters "Hello World", the program should display 3.
 # the vowels are a, e, i, o, u
