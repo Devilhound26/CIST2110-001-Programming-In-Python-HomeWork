1ed4067e0d5ba66682731199ae8ef4a32c46dcce
Jonathan welch <welchj2@go.stockton.edu>
2023-10-17 01:26:28 -0400

    finish project1

--- Project1.py
+++ Project1.py
@@ -26,8 +26,8 @@ print("Good luck!")
 # If the answer is incorrect, display a negative feedback message and provide the correct answer.
 # Score Tracking:
 print("Question 1: What is the capital of California?")
-print("a. San Francisco")
-print("b. Los Angeles")
+print("a. Los Angeles")
+print("b. San Francisco")
 print("c. Sacramento")
 print("d. San Diego")
 answer1 = input("Answer: ")
@@ -37,7 +37,8 @@ if answer1 == "c":
 else:
     print("Incorrect. The correct answer is c.")
     score = 0
-print("Question 2: What is the capital of Texas?")
+
+print("Question 2: What is the capital of Texas?")  
 print("a. Houston")
 print("b. Dallas")
 print("c. Austin")
@@ -81,17 +82,21 @@ print("d. Olympia")
 answer5 = input("Answer: ")
 if answer5 == "d":
     print("Great job!")
-    score = score + 1
+    score += 1
+    last_question_correct = True
 else:
     print("Incorrect. The correct answer is d.")
-    score = score + 0
-
-
+    last_question_correct = False
 # Keep track of the user's score throughout the game.
 # After all questions have been answered, display the user's total score and a farewell message.
 # Function Utilization:
-print("Your score is " + str(score) + " out of 5.")
+if last_question_correct:
+    print("Your score is " + str(score) + " out of 5.")
+else:
+    print("Your score is " + str(score) + " out of 5. Better luck next time!")
 print("Thank you for playing!")
+
+
 
 # Create a function to ask a question and check the answer. This function should accept parameters like the question, options, and the correct answer, and return whether the user was correct.
 # an example would be def ask_question(question, option_1, option_2, option_3, option_4, correct_answer):
@@ -118,9 +123,140 @@ else:
 
 
 # Create a function to display the final score, which takes the score as a parameter and displays a message.
+    def display_score(score):
+        print("Your score is " + str(score) + " out of 5.")
+        print("Thank you for playing!")
+
 # Loops:
-# Use a for or while loop to iterate through the questions.
+# Use a for or while loop to iterate through the questions
+while True:
+    print("Question 1: What is the capital of California?")
+    print("a. Los Angeles")
+    print("b. San Francisco")
+    print("c. Sacramento")
+    print("d. San Diego")
+    answer1 = input("Answer: ")
+    if answer1 == "c":
+        print("Wrong! just kidding! You got it right!")
+        score = 1
+    else:
+        print("Incorrect. The correct answer is c.")
+        score = 0
+    
+    print("Question 2: What is the capital of Texas?")
+    print("a. Houston")
+    print("b. Dallas")
+    print("c. Austin")
+    print("d. San Antonio")
+    answer2 = input("Answer: ")
+    if answer2 == "c":
+        print("Got to be the smartest person ever!")
+        score = score + 1
+    else:
+        print("Incorrect. The correct answer is c.")
+        score = score + 0
+    print("Question 3: What is the capital of New York?")
+    print("a. New York")
+    print("b. Albany")
+    print("c. Buffalo")
+    print("d. Syracuse")
+    answer3 = input("Answer: ")
+    if answer3 == "b":
+        print("Wow you got the answer right!")
+        score = score + 1
+    else:
+        print("Incorrect. The correct answer is b.")
+        score = score + 0
+    print("Question 4: What is the capital of Florida?")
+    print("a. Miami")
+    print("b. Orlando")
+    print("c. Tampa")
+    print("d. Tallahassee")
+    answer4 = input("Answer: ")
+    if answer4 == "d":
+        print("Correct!")
+        score = score + 1
+    else:
+        print("Incorrect. The correct answer is d.")
+        score = score + 0
+    print("Question 5: What is the capital of Washington?")
+    print("a. Seattle")
+    print("b. Spokane")
+    print("c. Tacoma")
+    print("d. Olympia")
+    answer5 = input("Answer: ")
+    if answer5 == "d":
+        print("Great job!")
+        score += 1
+        last_question_correct = True
+    else:
+        print("Incorrect. The correct answer is d.")
+        last_question_correct = False
+    
+    stop = input("Would you like to play again? (y/n) ")
+    
+    if stop == "n":
+        break
+        
+  
 # Variable Casting:
 # Ensure that user input is cast and checked appropriately to avoid errors during execution.
 # Error Handling:
-# Implement basic error handling to manage invalid inputs from the user (e.g., an answer other than a, b, c, or d).
+# Implement basic error handling to manage invalid inputs from the user (e.g., an answer other than a, b, c, or d)
+while True:
+    try:
+        answer1 = float(input("Answer: "))
+        if answer1 == "c":
+            print("Wrong! just kidding! You got it right!")
+            score = 1
+        else:
+            print("Incorrect. The correct answer is c.")
+            score = 0
+    except ValueError: 
+        pass
+
+    try:
+        answer2 = float(input("Answer: "))
+        if answer2 == "c":
+            print("Got to be the smartest person ever!")
+            score = score + 1
+        else:
+            print("Incorrect. The correct answer is c.")
+            score = score + 0
+    except ValueError:
+        pass
+    
+    try:
+        answer3 = float(input("Answer: "))   
+        if answer3 == "b":
+            print("Wow you got the answer right!")
+            score = score + 1
+        else:    
+            print("Incorrect. The correct answer is b.")
+            score = score + 0
+    except ValueError:
+        pass
+    
+    try:
+        answer4 = float(input("Answer: "))
+        if answer4 == "d":
+            print("Correct!")
+            score = score + 1
+        else:
+            print("Incorrect. The correct answer is d.")
+            score = score + 0
+    except ValueError:
+        pass
+        
+    try:
+        answer5 = float(input("Answer: "))
+        if answer5 == "d":
+            print("Great job!")
+            score += 1
+            last_question_correct = True
+        else:
+            print("Incorrect. The correct answer is d.")
+    except ValueError:
+        pass    
+        
+        break
